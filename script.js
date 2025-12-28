30b168893ac72a2050e888a56afa083094c8a371
su56-adcsm-f24 <51973180-su56-adcsm-f24@users.noreply.replit.com>
2025-12-28 10:33:11 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,63 @@
+let allTasks = [];
+function getTask(){
+  const title = document.getElementById('title-input').value;
+  const date = document.getElementById('date-input').value;
+  const priority = document.getElementById('priority-input').value;
+  if(!title || !date || !priority){
+     alert('Please fill all fields');
+     return null;
+    }
+  document.getElementById('title-input').value='';
+  document.getElementById('date-input').value='';
+  document.getElementById('priority-input').value='';
+console.log(title,date,priority);
+  return {id:Date.now()+Math.random(),title,date,priority,isCompleted:false}
+}
+document. getElementById('add-btn').addEventListener('click',()=>{
+  const task = getTask();
+  if(task){
+    allTasks.push(task);
+    renderTasks();
+  }
+});
+function renderTasks()
+{
+const high= document.getElementById('high-list');
+   const medium= document.getElementById('med-list');
+   const low= document.getElementById('low-list');
+  low.innerHTML='';
+  medium.innerHTML='';
+   high.innerHTML='';
+   allTasks.forEach(task=>{
+     const taskHTML=`
+     <div class="task-card ${task.isCompleted ? 'completed' : ''}">
+      <div class = "task-info">
+      <h3>${task.title}</h3>
+      <span class="task-date">${task.date}</span>
+    </div>
+    <div class="task-actions">
+      <button onclick="toggleTask(${task.id})">${task.isCompleted?'undo':'Done'}</button>
+      <button   class ="btn-delete" onclick="deleteTask(${task.id})">Delete</button>
+    </div>
+  </div>
+     `;
+   if(task.priority==='high')
+     { high.innerHTML+=taskHTML;}
+else if(task.priority==='medium')
+     { medium.innerHTML+=taskHTML;}
+else if(task.priority==='low')
+     { low.innerHTML+=taskHTML;}
+     });}
+function toggleTask(id){
+  allTasks=allTasks.map(task=>{
+    if(task.id===id){
+      task.isCompleted=!task.isCompleted;
+    }
+    return task;
+  });
+  renderTasks();
+}
+function deleteTask(id){
+   allTasks=allTasks.filter(task=>task.id!==id);
+   renderTasks();
+   }
